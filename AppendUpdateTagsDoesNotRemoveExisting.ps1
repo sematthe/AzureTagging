4e972e40da2136c0fdb6881637eb40f6faf25ac4
sematthe <40570815+sematthe@users.noreply.github.com>
2018-12-05 12:22:34 -0500

    elemental lessoxygen variable update

--- AppendUpdateTagsDoesNotRemoveExisting.ps1
+++ AppendUpdateTagsDoesNotRemoveExisting.ps1
@@ -8,7 +8,7 @@ $tagsToAdd = @{
     costCenter  = '56789'
     environment = 'development' 
     systemRole  = 'database'
-    elemental = 'oxygen'
+    elemental = 'lessoxygen'
 }
 # List of resoure groups to update
 $resourceGroupList = @("spoke-a", "hub")
